183f2d6acbb7af3644bef6519bbd1d41c969e3a5
khrr042 <khrr0420@gmail.com>
2026-01-26 21:34:33 +0900

    c

--- lib/data/datasets/__init__.py
+++ lib/data/datasets/__init__.py
@@ -1,17 +1,11 @@
-# encoding: utf-8
-from .veri import VeRi
-from .aicity20 import AICity20
-
+from .cityflow import CityFlow
 
 __factory = {
-    'veri': VeRi,
-    'aicity20': AICity20,
+    'cityflow': CityFlow,
 }
-
 
 def get_names():
     return __factory.keys()
-
 
 def init_dataset(name, *args, **kwargs):
     if name not in __factory.keys():
--- lib/data/datasets/cityflow.py
+++ lib/data/datasets/cityflow.py
@@ -1,80 +1,67 @@
 import os.path as osp
-import glob
 import re
+import xml.etree.ElementTree as ET
 from .bases import BaseImageDataset
 
 class CityFlow(BaseImageDataset):
-    """
-    CityFlowV2 Dataset
-    
-    Dataset structure:
-    data/
-        CityFlowV2/
-            image_train/
-            image_query/
-            image_test/
-            name_train.txt
-            name_query.txt
-            name_test.txt
-    """
-    dataset_dir = 'CityFlowV2'
+    dataset_dir = 'AIC21_Track2_ReID'
 
     def __init__(self, root='./data', verbose=True, **kwargs):
-        super(CityFlow, self).__init__()
+        super().__init__()
         self.dataset_dir = osp.join(root, self.dataset_dir)
+
         self.train_dir = osp.join(self.dataset_dir, 'image_train')
         self.query_dir = osp.join(self.dataset_dir, 'image_query')
         self.gallery_dir = osp.join(self.dataset_dir, 'image_test')
 
+        self.train_label_path = osp.join(self.dataset_dir, 'train_label.xml')
+        self.query_label_path = osp.join(self.dataset_dir, 'query_label.xml')
+        self.gallery_label_path = osp.join(self.dataset_dir, 'test_label.xml')
+
         self._check_before_run()
 
-        train = self._process_dir(self.train_dir, is_train=True)
-        query = self._process_dir(self.query_dir, is_train=False)
-        gallery = self._process_dir(self.gallery_dir, is_train=False)
+        train = self._process_dir_xml(self.train_dir, self.train_label_path, relabel=True)
+        query = self._process_dir_xml(self.query_dir, self.query_label_path, relabel=False)
+        gallery = self._process_dir_xml(self.gallery_dir, self.gallery_label_path, relabel=False)
 
         if verbose:
-            print("=> CityFlowV2 loaded")
+            print(f"=> AIC21 Track2 Loaded: {self.dataset_dir}")
             self.print_dataset_statistics(train, query, gallery)
 
-        self.train = train
-        self.query = query
-        self.gallery = gallery
+        self.train, self.query, self.gallery = train, query, gallery
 
-        self.num_train_pids, self.num_train_imgs, self.num_train_cams, self.num_train_vids = self.get_imagedata_info(self.train)
-        self.num_query_pids, self.num_query_imgs, self.num_query_cams, self.num_query_vids = self.get_imagedata_info(self.query)
-        self.num_gallery_pids, self.num_gallery_imgs, self.num_gallery_cams, self.num_gallery_vids = self.get_imagedata_info(self.gallery)
+        self.num_train_pids, self.num_train_imgs, self.num_train_cams, _ = self.get_imagedata_info(self.train)
+        self.num_query_pids, self.num_query_imgs, self.num_query_cams, _ = self.get_imagedata_info(self.query)
+        self.num_gallery_pids, self.num_gallery_imgs, self.num_gallery_cams, _ = self.get_imagedata_info(self.gallery)
 
     def _check_before_run(self):
-        """Check if all files are available before going deeper"""
-        if not osp.exists(self.dataset_dir):
-            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
-        if not osp.exists(self.train_dir):
-            raise RuntimeError("'{}' is not available".format(self.train_dir))
-        if not osp.exists(self.query_dir):
-            raise RuntimeError("'{}' is not available".format(self.query_dir))
-        if not osp.exists(self.gallery_dir):
-            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+        required = [
+            self.dataset_dir,
+            self.train_dir, self.query_dir, self.gallery_dir,
+            self.train_label_path, self.query_label_path, self.gallery_label_path,
+        ]
+        for p in required:
+            if not osp.exists(p):
+                raise RuntimeError(f"Missing: {p}")
 
-    def _process_dir(self, dir_path, is_train=True):
-        img_paths = sorted(glob.glob(osp.join(dir_path, '*.jpg')))
-        pattern = re.compile(r'(\d+)_c(\d+)')
+    def _process_dir_xml(self, img_dir, label_path, relabel=False):
+        dataset = []
+        tree = ET.parse(label_path)
+        items = tree.find('Items')
+        if items is None:
+            raise RuntimeError(f"Invalid XML format (Items not found): {label_path}")
 
-        data = []
-        for img_path in img_paths:
-            img_name = osp.basename(img_path)
-            
-            match = pattern.search(img_name)
-            
-            if match:
-                pid, camid = map(int, match.groups())
-                
-                if is_train:
-                    pid -= 1
-                camid -= 1
+        for obj in items:
+            image_name = obj.attrib['imageName']
+            img_path = osp.join(img_dir, image_name)
+            if not osp.exists(img_path):
+                raise FileNotFoundError(f"Image not found: {img_path}")
 
-                data.append((img_path, pid, camid))
-            else:
-                print(f"Warning: Skipping file with unexpected format: {img_name}")
-                continue
+            pid = int(obj.attrib['vehicleID'])
 
-        return data
+            cam_raw = obj.attrib.get('cameraID', '0')
+            m = re.findall(r'\d+', cam_raw)
+            camid = int(m[0]) if m else 0
+
+            dataset.append((img_path, pid, camid))
+        return dataset
